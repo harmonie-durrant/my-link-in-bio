ee153670ba1846873c04b7ca624e8d9a38d45127
Harmonie ✨ <r.bryenton2003@gmail.com>
2024-05-13 19:04:24 +0000

    Add wiki42

--- pages/index.js
+++ pages/index.js
@@ -75,11 +75,11 @@ export default function Home() {
                     </button>
                 </a>
                 <a
-                    href="https://blog.harmoniedurrant.com"
+                    href="https://wiki42.harmoniedurrant.com"
                     className="mb-4 w-[80%] h-auto flex justify-center items-center"
                 >
-                    <button type="button" className="btn w-full text-white bg-gradient-to-r from-[#F2B5D4] to-[#F7D6E0] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-purple-800">
-                        Harmonie{"'"}s World (My Blog)
+                    <button type="button" className="btn w-full text-white bg-gradient-to-r from-[#222222] to-[#444444] hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-purple-800">
+                        Wiki42
                     </button>
                 </a>
                 <a
