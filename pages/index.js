1c532530901a5641a6084facf84ca4606e503c77
Harmonie Durrant <r.bryenton2003@gmail.com>
2025-04-13 17:17:35 +0000

    Add new personal rojects

--- pages/index.js
+++ pages/index.js
@@ -68,6 +68,9 @@ export default function Home() {
                         <CategoryDividor name="Projects" />
                     </div>
                     <div className="collapse-content">
+
+                        <LinkButton name="Infinite Minesweeper (S&Box)" url="https://github.com/harmonie-durrant/3d-infinite-minesweeper" />
+                        <LinkButton name="HHA Cards (Home Assistant)" url="https://github.com/harmonie-durrant/hha-cards" />
                         <LinkButton name="Wiki42" url="https://wiki42.harmoniedurrant.com" />
                         <LinkButton name="Ash Smoked Fishes" url="https://www.ashsmokedfishes.co.uk/" />
                         <LinkButton name="Clickn' Circles" url="https://harmonie-durrant.itch.io/clickn-circles" />
